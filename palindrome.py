d772be7b3f3ec68fcdab05cbbdbe18cf5be7a9e8
msk007 <krawiec515@gmail.com>
2015-09-03 00:31:52 -0400

    Palindrome HW
    this is what i came up with for the Palindrome HW. tried a different
    way of comparing sentences

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,25 @@
-def is_palindrome(sentence):
-    # TODO: return True or False if the sentence is or isn't a palindrome
-    pass
+import re
 
+
+
+# TODO: return True or False if the sentence is or isn't a palindrome
+def is_palindrome(sentence):
+    sentence = re.sub(r'[^A-Za-z]', "", (sentence.lower()))
+    sentence_rev = sentence[::-1]
+    if sentence == sentence_rev:
+         return ("True")
+    else:
+         return ("False")
 
 def main():
-    # TODO: put your input/output code here
-    pass
+    sentence =input("What is your sentence? ")
+    is_palindrome(sentence)
+    check = is_palindrome(sentence)
+    if check:
+       print("{} is a palindrome".format(sentence))
+    else:
+       print("{} is not a palindrome".format(sentence))
 
 
 if __name__ == '__main__':
-    main()
+   main()
